fc64dafa5dc7d0f80126c05f5c7ad6f96d515154
Romain LEMBO <romain.lembo@calldesk.fr>
2021-12-19 17:06:54 +0100

    ant init ok

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import type { AppProps } from 'next/app'
 
+import { Button, DatePicker, version } from 'antd';
+
 import '../styles/globals.less';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="App">
+      <h1>antd version: {version}</h1>
+      <DatePicker />
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
+    </div>
+  );
 }
 
 export default MyApp
