aa9b4d7fb7c26fa4bcb9120f1f9fc532541b0dc6
Romain LEMBO <romain.lembo@calldesk.fr>
2021-12-19 17:00:13 +0100

    implement next x antd config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+// next.config.js
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  lessVarsFilePath: './styles/variables.less',
+  cssLoaderOptions: {
+    mode: 'local',
+    localIdentName: '[path][name]__[local]--[hash:base64:5]', // invalid! for Unify getLocalIdent (Next.js / CRA), Cannot set it, but you can rewritten getLocalIdentFn
+    exportLocalsConvention: 'camelCase',
+    exportOnlyLocals: false,
+  },
+
+  // Other Config Here...
+  webpack(config) {
+    return config;
+  },
+});
