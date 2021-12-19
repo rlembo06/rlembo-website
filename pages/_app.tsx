9062a8fc1e5bcee48c6a07d0183dd9f22367eb92
Romain LEMBO <romain.lembo@calldesk.fr>
2021-12-19 17:22:05 +0100

    according next example with antd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,7 @@
-import type { AppProps } from 'next/app'
-
-import { Button, DatePicker, version } from 'antd';
+import type { AppProps } from 'next/app';
 
 import '../styles/globals.less';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div className="App">
-      <h1>antd version: {version}</h1>
-      <DatePicker />
-      <Button type="primary" style={{ marginLeft: 8 }}>
-        Primary Button
-      </Button>
-    </div>
-  );
-}
+const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
-export default MyApp
+export default App;
